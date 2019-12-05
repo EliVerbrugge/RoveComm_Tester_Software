b238506dd5c9d105bd2e7404d3fa59d3a7b38448
Andrew Van Horn <andrewvh7@gmail.com>
2019-12-05 17:05:52 -0600

    Created Sender Widget

--- RoveComm_Tester/SenderWidget.Designer.cs
+++ RoveComm_Tester/SenderWidget.Designer.cs
@@ -28,39 +28,167 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel7 = new System.Windows.Forms.TableLayoutPanel();
+            this.MoveUp_pb = new System.Windows.Forms.Button();
+            this.MoveDown_pb = new System.Windows.Forms.Button();
+            this.Dubplicate_tb = new System.Windows.Forms.Button();
+            this.Number_lb = new System.Windows.Forms.Label();
+            this.Add_pb = new System.Windows.Forms.Button();
+            this.Delete_pb = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Settings_pb = new System.Windows.Forms.Button();
+            this.Send_pb = new System.Windows.Forms.Button();
+            this.tableLayoutPanel7.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tableLayoutPanel1
+            // tableLayoutPanel7
             // 
-            this.tableLayoutPanel1.ColumnCount = 4;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 1;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(802, 77);
-            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel7.ColumnCount = 9;
+            this.tableLayoutPanel7.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 44F));
+            this.tableLayoutPanel7.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 44F));
+            this.tableLayoutPanel7.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 44F));
+            this.tableLayoutPanel7.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 44F));
+            this.tableLayoutPanel7.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 44F));
+            this.tableLayoutPanel7.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 44F));
+            this.tableLayoutPanel7.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel7.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 44F));
+            this.tableLayoutPanel7.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 88F));
+            this.tableLayoutPanel7.Controls.Add(this.Send_pb, 8, 0);
+            this.tableLayoutPanel7.Controls.Add(this.Delete_pb, 0, 0);
+            this.tableLayoutPanel7.Controls.Add(this.Add_pb, 1, 0);
+            this.tableLayoutPanel7.Controls.Add(this.MoveDown_pb, 5, 0);
+            this.tableLayoutPanel7.Controls.Add(this.Number_lb, 0, 0);
+            this.tableLayoutPanel7.Controls.Add(this.Dubplicate_tb, 3, 0);
+            this.tableLayoutPanel7.Controls.Add(this.MoveUp_pb, 4, 0);
+            this.tableLayoutPanel7.Controls.Add(this.label1, 6, 0);
+            this.tableLayoutPanel7.Controls.Add(this.Settings_pb, 7, 0);
+            this.tableLayoutPanel7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel7.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel7.Name = "tableLayoutPanel7";
+            this.tableLayoutPanel7.RowCount = 2;
+            this.tableLayoutPanel7.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 44F));
+            this.tableLayoutPanel7.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel7.Size = new System.Drawing.Size(486, 58);
+            this.tableLayoutPanel7.TabIndex = 1;
+            // 
+            // MoveUp_pb
+            // 
+            this.MoveUp_pb.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MoveUp_pb.Location = new System.Drawing.Point(179, 3);
+            this.MoveUp_pb.Name = "MoveUp_pb";
+            this.MoveUp_pb.Size = new System.Drawing.Size(38, 38);
+            this.MoveUp_pb.TabIndex = 2;
+            this.MoveUp_pb.Text = "^";
+            this.MoveUp_pb.UseVisualStyleBackColor = true;
+            // 
+            // MoveDown_pb
+            // 
+            this.MoveDown_pb.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MoveDown_pb.Location = new System.Drawing.Point(223, 3);
+            this.MoveDown_pb.Name = "MoveDown_pb";
+            this.MoveDown_pb.Size = new System.Drawing.Size(38, 38);
+            this.MoveDown_pb.TabIndex = 3;
+            this.MoveDown_pb.Text = "V";
+            this.MoveDown_pb.UseVisualStyleBackColor = true;
+            // 
+            // Dubplicate_tb
+            // 
+            this.Dubplicate_tb.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Dubplicate_tb.Location = new System.Drawing.Point(135, 3);
+            this.Dubplicate_tb.Name = "Dubplicate_tb";
+            this.Dubplicate_tb.Size = new System.Drawing.Size(38, 38);
+            this.Dubplicate_tb.TabIndex = 4;
+            this.Dubplicate_tb.Text = "D";
+            this.Dubplicate_tb.UseVisualStyleBackColor = true;
+            // 
+            // Number_lb
+            // 
+            this.Number_lb.AutoSize = true;
+            this.Number_lb.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Number_lb.Location = new System.Drawing.Point(47, 0);
+            this.Number_lb.Name = "Number_lb";
+            this.Number_lb.Size = new System.Drawing.Size(38, 44);
+            this.Number_lb.TabIndex = 5;
+            this.Number_lb.Text = "1.";
+            this.Number_lb.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Add_pb
+            // 
+            this.Add_pb.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Add_pb.Location = new System.Drawing.Point(91, 3);
+            this.Add_pb.Name = "Add_pb";
+            this.Add_pb.Size = new System.Drawing.Size(38, 38);
+            this.Add_pb.TabIndex = 6;
+            this.Add_pb.Text = "+";
+            this.Add_pb.UseVisualStyleBackColor = true;
+            // 
+            // Delete_pb
+            // 
+            this.Delete_pb.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Delete_pb.Location = new System.Drawing.Point(3, 3);
+            this.Delete_pb.Name = "Delete_pb";
+            this.Delete_pb.Size = new System.Drawing.Size(38, 38);
+            this.Delete_pb.TabIndex = 7;
+            this.Delete_pb.Text = "X";
+            this.Delete_pb.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(267, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 44);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Settings_pb
+            // 
+            this.Settings_pb.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Settings_pb.Location = new System.Drawing.Point(357, 3);
+            this.Settings_pb.Name = "Settings_pb";
+            this.Settings_pb.Size = new System.Drawing.Size(38, 38);
+            this.Settings_pb.TabIndex = 9;
+            this.Settings_pb.Text = "O";
+            this.Settings_pb.UseVisualStyleBackColor = true;
+            // 
+            // Send_pb
+            // 
+            this.Send_pb.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Send_pb.Location = new System.Drawing.Point(401, 3);
+            this.Send_pb.Name = "Send_pb";
+            this.Send_pb.Size = new System.Drawing.Size(82, 38);
+            this.Send_pb.TabIndex = 10;
+            this.Send_pb.Text = "Send";
+            this.Send_pb.UseVisualStyleBackColor = true;
             // 
             // SenderWidget
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(802, 77);
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.ClientSize = new System.Drawing.Size(486, 58);
+            this.Controls.Add(this.tableLayoutPanel7);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "SenderWidget";
             this.Text = "SenderWidget";
+            this.tableLayoutPanel7.ResumeLayout(false);
+            this.tableLayoutPanel7.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel7;
+        private System.Windows.Forms.Button Send_pb;
+        private System.Windows.Forms.Button Delete_pb;
+        private System.Windows.Forms.Button Add_pb;
+        private System.Windows.Forms.Button MoveDown_pb;
+        private System.Windows.Forms.Label Number_lb;
+        private System.Windows.Forms.Button Dubplicate_tb;
+        private System.Windows.Forms.Button MoveUp_pb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Settings_pb;
     }
 }
--- RoveComm_Tester/SenderWidget.cs
+++ RoveComm_Tester/SenderWidget.cs
@@ -16,5 +16,15 @@ namespace RoveComm_Tester
         {
             InitializeComponent();
         }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
